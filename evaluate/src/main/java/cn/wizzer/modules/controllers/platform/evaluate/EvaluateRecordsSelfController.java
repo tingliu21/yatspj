586bbfded2c0a7a40f56c5f391f99be5c8d321ee
tingliu21 <tingliu21@gmail.com>
2018-12-21 07:44:21 +0800

    4次被覆盖的修改重新提交
    3bd8a243 初始化自评
    5b8292c8 加入poi库
    22464d7c 增加基础性指标、规范性指标、发展性指标、下载四个页面
    f8f67085 增加下载功能

--- evaluate/src/main/java/cn/wizzer/modules/controllers/platform/evaluate/EvaluateRecordsSelfController.java
+++ evaluate/src/main/java/cn/wizzer/modules/controllers/platform/evaluate/EvaluateRecordsSelfController.java
@@ -19,6 +19,7 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.subject.Subject;
 import org.nutz.dao.Cnd;
+import org.nutz.dao.entity.Record;
 import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.lang.Strings;
@@ -27,7 +28,12 @@ import org.nutz.log.Logs;
 import org.nutz.mvc.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @IocBean
 @At("/platform/evaluate/records/self")
@@ -53,7 +59,33 @@ public class EvaluateRecordsSelfController {
 	public void index() {
 
 	}
+	@At
+	@Ok("beetl:/platform/evaluate/records/self/index_basic.html")
+	@RequiresAuthentication
+	public void index_basic() {
 
+	}
+
+	@At
+	@Ok("beetl:/platform/evaluate/records/self/index_standard.html")
+	@RequiresAuthentication
+	public void index_standard() {
+
+	}
+
+	@At
+	@Ok("beetl:/platform/evaluate/records/self/index_develop.html")
+	@RequiresAuthentication
+	public void index_develop() {
+
+	}
+
+	@At
+	@Ok("beetl:/platform/evaluate/records/self/index_download.html")
+	@RequiresAuthentication
+	public void index_download() {
+
+	}
 	//学校自评列表
 	@At
 	@Ok("json:full")
@@ -180,25 +212,36 @@ public class EvaluateRecordsSelfController {
     }
 
 
-    @At({"/delete","/delete/?"})
-    @Ok("json")
-    @SLog(tag = "删除Evaluate_records_self", msg = "ID:${args[2].getAttribute('id')}")
-    public Object delete(String id, @Param("ids") String[] ids ,HttpServletRequest req) {
+//    @At({"/delete","/delete/?"})
+//    @Ok("json")
+//    @SLog(tag = "删除Evaluate_records_self", msg = "ID:${args[2].getAttribute('id')}")
+//    public Object delete(String id, @Param("ids") String[] ids ,HttpServletRequest req) {
+//		try {
+//			if(ids!=null&&ids.length>0){
+//				evaluateRecordsSelfService.delete(ids);
+//    			req.setAttribute("id", org.apache.shiro.util.StringUtils.toString(ids));
+//			}else{
+//				evaluateRecordsSelfService.delete(id);
+//    			req.setAttribute("id", id);
+//			}
+//			return Result.success("system.success");
+//		} catch (Exception e) {
+//			return Result.error("system.error");
+//		}
+//    }
+
+	@At({"/delete","/delete/?"})
+	@Ok("json")
+	@SLog(tag = "删除Evaluate_records_self", msg = "ID:${args[2].getAttribute('id')}")
+	public Object delete(String id ,HttpServletRequest req) {
 		try {
-			if(ids!=null&&ids.length>0){
-				evaluateRecordsSelfService.delete(ids);
-    			req.setAttribute("id", org.apache.shiro.util.StringUtils.toString(ids));
-			}else{
-				evaluateRecordsSelfService.delete(id);
-    			req.setAttribute("id", id);
-			}
+			evaluateRecordsSelfService.deleteAndChild(id);
+			//req.setAttribute("id", id);
 			return Result.success("system.success");
 		} catch (Exception e) {
 			return Result.error("system.error");
 		}
-    }
-
-
+	}
     @At("/detail/?")
     @Ok("beetl:/platform/evaluate/records/detail.html")
     @RequiresAuthentication
@@ -209,6 +252,105 @@ public class EvaluateRecordsSelfController {
 		}
 		return null;
     }
-	//endregion
+	@At("/download/?")
+	@Ok("void")
+	@RequiresAuthentication
+	public Object download(String id, HttpServletResponse resp) {
+		if (!Strings.isBlank(id)) {
+			Map<String,Object> wordDataMap = packageObject(id);
+			XwpfUtil xwpfUtil = new XwpfUtil();
+			//读入word模板
+			InputStream is = getClass().getClassLoader().getResourceAsStream("template/SelfEvaluate.docx");
+			try {
+				String filename = "拱墅区现代优质学校评估自评表.docx";
+				filename = URLEncoder.encode(filename, "UTF-8");
+
+				xwpfUtil.exportWord(wordDataMap,is,resp,filename);
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		return null;
+	}
+
+	/**
+	 * 组装word文档中需要显示数据的集合
+	 * @return
+	 */
+	public Map<String, Object> packageObject(String evalId) {
+		Map<String,Object> wordDataMap = new HashMap<String,Object>();
+		Map<String, Object> parametersMap = new HashMap<String, Object>();
+
+		List<Map<String, Object>> table_scale = new ArrayList<Map<String, Object>>();
+
+		List<Record> recordsUnitInfo = evaluateRecordsSelfService.getUnitInfo(evalId);
+		if (recordsUnitInfo.size() > 0) {
+			parametersMap.put("unitname", recordsUnitInfo.get(0).getString("unitname"));
+			parametersMap.put("address", recordsUnitInfo.get(0).getString("address"));
+			parametersMap.put("website", recordsUnitInfo.get(0).getString("website"));
+			parametersMap.put("telephone", recordsUnitInfo.get(0).getString("telephone"));
+			parametersMap.put("email", recordsUnitInfo.get(0).getString("email"));
+		}
+
+		List<Record> recordsBasicEvalData = evaluateRecordsSelfService.getBasicEvalData(evalId);
+		for (Record record : recordsBasicEvalData) {
+			int location = record.getInt("location");
+			if (record.get("qualify") != null) {
+				if ((boolean) record.get("qualify"))
+					parametersMap.put("s_i" + location, "是");
+				else
+					parametersMap.put("s_i" + location, "否");
+			} else {
+				parametersMap.put("s_i" + location, "");
+			}
+		}
+
+		List<Record> recordsBasicSummaryData = evaluateRecordsSelfService.getBasicSummaryData(evalId);
+		for (Record record : recordsBasicSummaryData) {
+			int location = record.getInt("location");
+			parametersMap.put("sintro"+location, record.getString("summary"));
+		}
+
+		List<Record> recordsRemarkData = evaluateRecordsSelfService.getRemarkData(evalId);
+		for (Record record : recordsRemarkData) {
+			int location = record.getInt("location");
+			double score_s = record.getDouble("score_s");
+			double score_p = record.getDouble("score_p");
+			String remark_s = record.getString("remark_s");
+			parametersMap.put("s_i" + location, formatDouble(score_s));
+			parametersMap.put("p_i" + location, formatDouble(score_p));
+			parametersMap.put("r_i" + location, remark_s);
+		}
+
+		List<Record> recordsScaleData = evaluateRecordsSelfService.getScaleData(evalId);
+		for (Record record : recordsScaleData) {
+			Map<String, Object> map=new HashMap<>();
+			map.put("grade", record.getString("grade"));
+			map.put("plannum", record.getInt("planenrollnum"));
+			map.put("realnum", record.getInt("actualenrollnum"));
+			map.put("classnum", record.getInt("classnum"));
+			map.put("avgnum", record.getInt("averagenum"));
+			map.put("extra", record.getString("instruction"));
+			table_scale.add(map);
+		}
+
+
+		wordDataMap.put("table_scale", table_scale);
+		wordDataMap.put("parametersMap", parametersMap);
+		return wordDataMap;
+	}
+
+	public String formatDouble(double d) {
+		BigDecimal bg = new BigDecimal(d).setScale(1, RoundingMode.UP);
+		double num = bg.doubleValue();
+		if (Math.round(num) - num == 0) {
+			return String.valueOf((long) num);
+		}
+		return String.valueOf(num);
+	}
+
+//endregion
 
 }
--- evaluate/src/main/java/cn/wizzer/modules/services/evaluate/EvaluateRecordsSelfService.java
+++ evaluate/src/main/java/cn/wizzer/modules/services/evaluate/EvaluateRecordsSelfService.java
@@ -5,10 +5,13 @@ import cn.wizzer.modules.models.evaluate.Evaluate_records_self;
 import org.nutz.aop.interceptor.ioc.TransAop;
 import org.nutz.dao.Dao;
 import org.nutz.dao.Sqls;
+import org.nutz.dao.entity.Record;
 import org.nutz.ioc.aop.Aop;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.log.Log;
 import org.nutz.log.Logs;
+import java.util.List;
+
 @IocBean(args = {"refer:dao"})
 public class EvaluateRecordsSelfService extends Service<Evaluate_records_self> {
 	private static final Log log = Logs.get();
@@ -32,6 +35,7 @@ public class EvaluateRecordsSelfService extends Service<Evaluate_records_self> {
         dao().execute(Sqls.create("delete from evaluate_remark where evaluateid=@id ").setParam("id", evaluateId));
 
 
+
     }
     /**
      * 提交评估记录
@@ -43,5 +47,26 @@ public class EvaluateRecordsSelfService extends Service<Evaluate_records_self> {
 
         dao().execute(Sqls.create("update evaluate_records set selfeva = true where evaluateid=@id ").setParam("id", evaluateId));
     }
+    public List<Record> getUnitInfo(String evalId) {
+        return list(Sqls.create("select t1.name as unitname, t1.address as address, t1.website as website, t1.telephone as telephone, t1.email as email from sys_unit t1 join evaluate_records t2 on t1.id=t2.schoolid where t2.id=@id").setParam("id", evalId));
+
+    }
+
+    public List<Record> getBasicEvalData(String evalId) {
+        return list(Sqls.create("select t1.qualify as qualify, t2.location from evaluate_qualify t1 join monitor_index t2 on t1.indexid=t2.id where evaluateid=@id order by location").setParam("id", evalId));
+    }
+
+    public List<Record> getBasicSummaryData(String evalId) {
+        return list(Sqls.create("select t1.summary, t2.location from evaluate_summary t1 join monitor_catalog t2 on t1.catalogid=t2.id where evaluateid=@id order by location").setParam("id", evalId));
+    }
+
+    public List<Record> getRemarkData(String evalId) {
+        return list(Sqls.create("select t1.score_s, t1.remark_s, t1.score_p, t2.location from evaluate_remark t1 join monitor_index t2 on t1.indexid=t2.id where evaluateid=@id order by location").setParam("id", evalId));
+    }
+
+    public List<Record> getScaleData(String evalId) {
+        return list(Sqls.create("select t1.grade,t1.planenrollnum,t1.actualenrollnum, t1.classnum,t1.averagenum,t1.instruction from bascidata_scale t1 join evaluate_records t2 on t1.schoolid=t2.schoolid and t1.year=t2.year where t2.id=@id").setParam("id", evalId));
+    }
+
 }
 
