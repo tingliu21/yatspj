884c8d5daa828971c99fb6f59583dcf0e670fdfb
tingliu21 <tingliu21@gmail.com>
2018-12-24 21:34:38 +0800

    联络员权限

--- evaluate/src/main/java/cn/wizzer/modules/services/monitor/MonitorIndexService.java
+++ evaluate/src/main/java/cn/wizzer/modules/services/monitor/MonitorIndexService.java
@@ -26,7 +26,7 @@ public class MonitorIndexService extends Service<Monitor_index> {
     }
     //获取专家审核指标
     public List<Record> getSpecialIndex(String unittype){
-        Sql sql = Sqls.create("select index.id, sys_role.code as rolecode from monitor_index as index inner join sys_role on sys_role.id = index.masterrole inner join sys_unit on sys_unit.id = index.department where sys_unit.aliasname='Special' and monitor_index.unittype=@unittype").setParam("unittype", unittype);
+        Sql sql = Sqls.create("select index.id, sys_role.code as rolecode from monitor_index as index inner join sys_role on sys_role.id = index.masterrole inner join sys_unit on sys_unit.id = index.department where sys_unit.aliasname='Special' and index.unittype=@unittype").setParam("unittype", unittype);
         List<Record> indexlist = list(sql);
         return  indexlist;
 
